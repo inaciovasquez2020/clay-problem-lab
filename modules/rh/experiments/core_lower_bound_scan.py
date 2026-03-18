27b2607ac1b527c75be9ff0f14c5dea36b775b1a
Inacio F. Vasquez <inaciovasquez2020@gmail.com>
2026-03-18 12:35:21 -0400

    Add core-mass scan for AKCL lower-bound support

---
+++ modules/rh/experiments/core_lower_bound_scan.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+def weight(x):
+    return np.exp(-x)
+
+def f_sample(xs):
+    f = xs - np.mean(xs)
+    norm = np.sqrt(np.sum((f**2) * weight(xs)))
+    return f / norm
+
+def core_mass(xs, theta):
+    f = f_sample(xs)
+    core = xs <= theta * xs[-1]
+    return np.sum((f[core]**2) * weight(xs[core]))
+
+for L in [2, 3, 4, 5, 6, 8, 10]:
+    xs = np.linspace(0.5, L, 400)
+    cm = core_mass(xs, 0.8)
+    print("L =", L, "core_mass =", cm)
